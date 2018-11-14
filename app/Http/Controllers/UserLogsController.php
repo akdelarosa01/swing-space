3598e848ae2c37af84d6296d4c34d01edcb22e15
Kurt Dela Rosa <kurtcreampie@gmail.com>
2018-11-14 18:01:04 +0800

    fix user logs

--- app/Http/Controllers/UserLogsController.php
+++ app/Http/Controllers/UserLogsController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\GlobalController;
+use App\UserLog;
 
 class UserLogsController extends Controller
 {
-    //
+    protected $_global;
+
+    public function __construct()
+    {
+        $this->_global = new GlobalController;
+    }
+
+    public function log(array $params)
+    {
+    	UserLog::create($params);
+    }
 }
